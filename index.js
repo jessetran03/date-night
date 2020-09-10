4c4df3ffbc7c2872b5ede08ea4c51b90eb5c36d2
Jesse Tran <jessetran03@gmail.com>
2020-09-10 16:11:46 -0500

    Adjusted flexbox and cleaned code

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var dinnerPlan = {
 }
 
 var dessertPlan = {
-    id: 'id',
-    name: 'ShareTea'
+    id: 17355196,
+    name: 'Spark Cafe'
 }
 
 var currentLocation = {
@@ -24,35 +24,7 @@ const searchURL = 'https://developers.zomato.com/api/v2.1/search';
 const locationURL = 'https://developers.zomato.com/api/v2.1/locations'
 const weatherURL = 'https://api.weatherbit.io/v2.0/forecast/hourly'
 
-function renderDatePlan() {
-    $('.js-plan').html(
-        `<li item-id="${currentLocation.entityCode}">
-            <h5>Location</h5>
-            <p>${currentLocation.title}</span>
-        </li>
-        <li item-id="${dinnerPlan.id}">
-            <h5>Dinner</h5>
-            <p id="js-dinner">${dinnerPlan.name}</span>
-        </li>
-        <li item-id="${dessertPlan.id}">
-            <h5>Dessert</h5>
-            <p id="js-dessert">${dessertPlan.name}</span>
-        </li>`
-    );
-    $('.group').removeClass('hidden');
-    $('.initial').remove();
-}
-
-function handleItemSelect() {
-    $('.js-results').on('click', '.js-select-dinner', event => {
-        dinnerPlan.name = $(event.currentTarget).siblings('.restaurant-first').children('h4').text();
-        renderDatePlan();
-    });
-    $('.js-results').on('click', '.js-select-dessert', event => {
-        dessertPlan.name = $(event.currentTarget).siblings('.restaurant-first').children('h4').text();
-        renderDatePlan();
-    });
-}
+/*-----------------------FORMAT-FUNCTIONS----------------------*/
 
 function formatHours(rawHours) {
     let hours = rawHours.split(',');
@@ -60,7 +32,6 @@ function formatHours(rawHours) {
 }
 
 function formatTime(rawTime) {
-    //const time = rawTime.slice(rawTime.length - 8)
     let time = rawTime.substring(11, 13);
     time = parseInt(time);
     if (time === 0) {
@@ -84,8 +55,40 @@ function formatQueryParams(params) {
             return queryItems.join('&');
 }
 
-function displayResultsLocation(responseJson, searchTerm) {
-    currentLocation.title = responseJson.location_suggestions[0].title;
+/*-----------------------------------RENDER-FUNCTIONS------------------------------*/
+
+function renderDatePlan() {
+    $('.js-plan').html(
+        `<li item-id="${currentLocation.entityCode}">
+            <h5>Location</h5>
+            <p>${currentLocation.title}</span>
+        </li>
+        <li item-id="${dinnerPlan.id}">
+            <h5>Dinner</h5>
+            <p id="js-dinner">${dinnerPlan.name}</span>
+        </li>
+        <li item-id="${dessertPlan.id}">
+            <h5>Dessert</h5>
+            <p id="js-dessert">${dessertPlan.name}</span>
+        </li>`
+    );
+    $('.group, .title').removeClass('hidden');
+    $('.initial').remove();
+}
+
+function displayWeather(responseJson) {
+    console.log(responseJson);
+    $('.js-weather').empty();
+    for (let i = 0; i < 12; i = i + 2) {
+        const time = formatTime(responseJson.data[i].timestamp_local);
+        $('.js-weather').append(
+            `<li>${time}: ${responseJson.data[i].temp}°F</li>`
+        );
+    }
+    $('#js-weather').removeClass('hidden');
+}
+
+function displayResultsHeader(searchTerm) {
     let planItem = '';
     if (searchTerm === 5) {
         planItem = 'dessert';
@@ -93,7 +96,7 @@ function displayResultsLocation(responseJson, searchTerm) {
     else {
         planItem = 'dinner';
     }
-    $('.results h3').empty().append(`Displaying ${planItem} results for ${currentLocation.title}`);
+    $('.results h3').empty().append(`Displaying results for ${planItem} in ${currentLocation.title}`);
     $('.js-results').empty();
 }
 
@@ -127,20 +130,20 @@ function displayResults(responseJson, planItem) {
         );
     }
     $('.results').removeClass('hidden');
-    renderDatePlan();
 };
 
-function displayWeather(responseJson) {
+/*---------------------------------GET-FUNCTIONS-----------------------------*/
+
+function getLocationCode(responseJson, searchTerm) {
     console.log(responseJson);
-    $('.js-weather').empty();
-    for (let i = 0; i < 12; i = i + 2) {
-        const time = formatTime(responseJson.data[i].timestamp_local);
-        $('.js-weather').append(
-            `<li>${time}: ${responseJson.data[i].temp}°F</li>`
-        );
-    }
-    $('#js-weather').removeClass('hidden');
+    currentLocation.latitude = responseJson.location_suggestions[0].latitude;
+    currentLocation.longitude = responseJson.location_suggestions[0].longitude;
+    currentLocation.entityCode = responseJson.location_suggestions[0].entity_id;
+    currentLocation.entityType = responseJson.location_suggestions[0].entity_type;
+    currentLocation.title = responseJson.location_suggestions[0].title;
     renderDatePlan();
+    getWeather();
+    getRestaurants(searchTerm);
 }
 
 function getCity(searchCity, searchTerm) {
@@ -149,7 +152,6 @@ function getCity(searchCity, searchTerm) {
     }
     const queryString = formatQueryParams(params)
     const url = locationURL + '?' + queryString;
-
     const options = {
         headers: new Headers({
           "user-key": apiKey})
@@ -166,56 +168,6 @@ function getCity(searchCity, searchTerm) {
     .catch(err => {
         $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
-}
-
-function getLocationCode(responseJson, searchTerm) {
-    console.log(responseJson);
-    displayResultsLocation(responseJson, searchTerm);
-    currentLocation.latitude = responseJson.location_suggestions[0].latitude;
-    currentLocation.longitude = responseJson.location_suggestions[0].longitude;
-    currentLocation.entityCode = responseJson.location_suggestions[0].entity_id;
-    currentLocation.entityType = responseJson.location_suggestions[0].entity_type;
-    getWeather();
-    getRestaurants(searchTerm);
-}
-
-function getRestaurants(searchTerm) {
-    const params = {
-        entity_id: currentLocation.entityCode,
-        entity_type: currentLocation.entityType,
-        cuisines: searchTerm,
-        count: 25,
-        sort: 'rating'
-    };
-    const queryString = formatQueryParams(params)
-    const url = searchURL + '?' + queryString;
-
-    let planItem = '';
-    if (searchTerm === 5) {
-        planItem = 'dessert';
-    }
-    else {
-        planItem = 'dinner';
-    }
-
-    const options = {
-        headers: new Headers({
-          "user-key": apiKey})
-      };
-
-    fetch(url, options)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
-        .then(responseJson => {
-            displayResults(responseJson, planItem);
-        })
-        .catch(err => {
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);
-        });
 }
 
 function getWeather() {
@@ -243,53 +195,96 @@ function getWeather() {
         });
 }
 
+function getRestaurants(searchTerm) {
+    displayResultsHeader(searchTerm);
 
-//$('.js-results').on('click', '.js-select-dinner', event => {
- //   dinnerPlan.name = $(event.currentTarget).siblings('.restaurant-first').children('h4').text();
- //   renderDatePlan();
-//});
+    const params = {
+        entity_id: currentLocation.entityCode,
+        entity_type: currentLocation.entityType,
+        cuisines: searchTerm,
+        count: 25,
+        sort: 'rating'
+    };
+    const queryString = formatQueryParams(params)
+    const url = searchURL + '?' + queryString;
+    const options = {
+        headers: new Headers({
+          "user-key": apiKey})
+      };
+
+    let planItem = '';
+    if (searchTerm === 5) {
+        planItem = 'dessert';
+    }
+    else {
+        planItem = 'dinner';
+    }
+
+    fetch(url, options)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => {
+            displayResults(responseJson, planItem);
+        })
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+}
+
+/*------------------------------EVENT-LISTENERS-----------------------------------*/
+
+function handleItemSelect() {
+    $('.js-results').on('click', '.js-select-dinner', event => {
+        dinnerPlan.name = $(event.currentTarget).siblings('.restaurant-first').children('h4').text();
+        renderDatePlan();
+    });
+    $('.js-results').on('click', '.js-select-dessert', event => {
+        dessertPlan.name = $(event.currentTarget).siblings('.restaurant-first').children('h4').text();
+        renderDatePlan();
+    });
+}
+
+function watchInitialSearch() {
+    $('#js-initial-search').submit(event => {
+        event.preventDefault();
+        const searchCity = $('#js-city').val();
+        getCity(searchCity, 1);
+    });
+}
 
 function watchChangeLocation() {
-    $('body').on('click', '.js-change-location', event => {
+    $('#js-change-location').submit(event => {
         event.preventDefault();
         const searchCity = $('#js-city').val();
         getCity(searchCity, 5);
     });
 }
 
-function watchFormDessert() {
-    $('.group-two').on('click', '.js-find-dessert', event => {
-        event.preventDefault();
-        const searchCity = $('#js-city').val();
-        //getCity(searchCity, 5);
-        getRestaurants(5);
-    });
-}
-
-function watchDessert() {
-    $('.group-two').on('click', '#js-dessert', event => {
-        event.preventDefault();
-        const searchCity = $('#js-city').val();
-        getCity(searchCity, 5);
-    });
-}
-
-function watchForm() {
-    $('.group-two').on('click', '.js-find-dinner', event => {
+function watchDinner() {
+    $('.group-one').on('click', '.js-find-dinner', event => {
         event.preventDefault();
         const searchTerm = $('#js-cuisine').val();
-        const searchCity = $('#js-city').val();
-        //getCity(searchCity, searchTerm);
         getRestaurants(searchTerm);
     });
 }
 
+function watchDessert() {
+    $('.group-one').on('click', '.js-find-dessert', event => {
+        event.preventDefault();
+        getRestaurants(5);
+    });
+}
+
 function handleApp() {
-    watchForm();
-    watchFormDessert();
+    watchInitialSearch();
     watchChangeLocation();
-    handleItemSelect();
+    watchDinner();
     watchDessert();
+    handleItemSelect();
 }
 
 $(handleApp);
